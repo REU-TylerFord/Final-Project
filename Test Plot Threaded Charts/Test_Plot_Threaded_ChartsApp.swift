93dc619a3f2a7b4c55e8c893b3bc84089bff7534
Jeff Terry <terryj@iit.edu>
2022-08-25 11:00:23 -0500

    Initial Commit

---
+++ Test Plot Threaded Charts/Test_Plot_Threaded_ChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Plot_Threaded_ChartsApp.swift
+//  Test Plot Threaded Charts
+//
+//  Created by Jeff Terry on 8/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Test_Plot_Threaded_ChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
